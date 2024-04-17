206ac8f67ff198e9073b7c3fa62962cb989d8ff6
Droivox <droivox@outlook.com>
2024-04-17 13:10:00 -0300

    -902446

---
+++ git.py
@@ -0,0 +1,9 @@
+import random, os
+
+git_id = random.randint(-9999999, 999999)
+path = os.path.dirname(os.path.realpath(__file__))
+msg = str(git_id)
+
+os.system("cd " + str(path))
+os.system("git add .")
+os.system("git commit -m " + str(msg))
