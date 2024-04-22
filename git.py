479ea0b47e7da7ae5657293403734fdc05c2fd33
Droivox <droivox@outlook.com>
2024-04-21 23:42:02 -0300

    628372349714769

--- git.py
+++ git.py
@@ -1,9 +1,10 @@
 import random, os
 
 path = os.path.dirname(os.path.realpath(__file__))
-msg = input("Git -M : ")
+msg = str(random.randint(0, 999999999999999))
 
 os.system("cd " + str(path))
 os.system("git add .")
 os.system("git commit -m " + msg)
 os.system("git push")
+os.system("@pause")
