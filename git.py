a466d71bba1456cec95a9fc0a5998b9a9fac1fb4
neetgon <neetgon@outlook.com>
2025-10-02 20:31:32 -0300

    update

--- git.py
+++
@@ -1,9 +0,0 @@
-import random, os
-
-path = os.path.dirname(os.path.realpath(__file__))
-msg = str(random.randint(0, 999999999999999))
-
-os.system("cd " + str(path))
-os.system("git add .")
-os.system("git commit -m " + msg)
-os.system("git push")
