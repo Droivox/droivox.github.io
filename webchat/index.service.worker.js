896596928a06c7fdb51b9cfc5db10564a97e6f86
neetgon <neetgon@outlook.com>
2025-10-05 22:07:31 -0300

    update

--- webchat/index.service.worker.js
+++ webchat/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1759570997|458351063';
+const CACHE_VERSION = '1759712834|1067922068';
 /** @type {string} */
 const CACHE_PREFIX = 'Toon Shader-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
