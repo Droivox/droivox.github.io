78105fcb432ea22e552234ea6d64a88ee1e2916c
Gon <neetgon@outlook.com>
2025-11-08 14:09:38 -0300

    update 1.1.4

--- webchat/index.service.worker.js
+++ webchat/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1762619903|942913111';
+const CACHE_VERSION = '1762621761|2800861934';
 /** @type {string} */
 const CACHE_PREFIX = 'Web Chat Client-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
